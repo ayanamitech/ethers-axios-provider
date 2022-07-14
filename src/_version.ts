b64d4a049232b6b421461b4e5e5d9f2c0dd8b234
Ayanami <ayanami0330@protonmail.com>
2022-07-14 16:19:33 +0900

    Bump axios-auto version ( 5.6.20 )

--- src/_version.ts
+++ src/_version.ts
@@ -1,1 +1,1 @@
-export const version = 'ethers-axios-provider@5.6.19';
+export const version = 'ethers-axios-provider@5.6.20';
--- types/_version.d.ts
+++ types/_version.d.ts
@@ -1,1 +1,1 @@
-export declare const version = "ethers-axios-provider@5.6.19";
+export declare const version = "ethers-axios-provider@5.6.20";
